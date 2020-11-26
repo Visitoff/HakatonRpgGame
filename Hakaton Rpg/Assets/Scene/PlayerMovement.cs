babc5f88a5f84bdbb151168d98926493f8da4dc1
Visitoff <60969574+Visitoff@users.noreply.github.com>
2020-11-26 18:00:40 +0500

    Initial Commit

---
+++ Hakaton Rpg/Assets/Scene/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : CameraControl
+{
+    [SerializeField]
+    CharacterController controller;
+    public Transform playerBody;
+    
+    
+
+
+  
+
+    void Update()
+    {
+        PersonMovement();
+    }
+
+    void PersonMovement()
+    {
+        float movementX = Input.GetAxis("Horizontal");
+        movementX *= Time.deltaTime;
+        float movementY = Input.GetAxis("Vertical");
+        movementY *= Time.deltaTime;
+        Vector3 playerMove = transform.right * movementX + cam.transform.forward * movementY;
+        playerMove += Physics.gravity;
+        controller.Move(playerMove * 1000f * Time.deltaTime);
+    }
+
+   
+}
